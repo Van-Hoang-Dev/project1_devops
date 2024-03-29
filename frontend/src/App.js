9c452c04dbd1f9bec1082425c4cc0e605bc3554a
trkhang09 <dtrikhang0910@gmail.com>
2024-03-29 16:46:21 +0700

    delete import boostraps at App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Banner from './components/Banner';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
