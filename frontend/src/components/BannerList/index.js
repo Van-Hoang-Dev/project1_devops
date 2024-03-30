e9a84bec5b7e1be6179e0be84463e9ffb0eb4ecf
phuongnhi130920@gmail.com <phuongnhi130920@gmail.com>
2024-03-30 08:35:26 +0700

    update header-banner

--- frontend/src/components/BannerList/index.js
+++ frontend/src/components/BannerList/index.js
@@ -2,19 +2,17 @@
 function BannerList() {
     return (
         <>
-            <div className="carousel-item">
-                  <div className="container">
-                     <div className="row">
-                        <div className="col-sm-6">
-                           <h1 className="banner_taital">Beauty <br/>Kit</h1>
-                           <p className="banner_text">Ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
-                           <div className="read_bt"><a href="#">Buy Now</a></div>
-                        </div>
-                        <div className="col-sm-6">
-                           <div className="banner_img"><img src="images/banner-img.png"/></div>
-                        </div>
-                     </div>
-                  </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h1 className="banner_taital">Beauty <br />Kit</h1>
+                        <p className="banner_text">Ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                        <div className="read_bt"><a href="#">Buy Now</a></div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="banner_img"><img src="images/banner-img.png" /></div>
+                    </div>
+                </div>
             </div>
         </>
     )
