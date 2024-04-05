36dfe5f9f7bc3c1c9badaaad192593ba2d1e594e
Van-Hoang-Dev <vanhoang24edu@gmail.com>
2024-04-05 22:50:40 +0700

    "Push"

--- frontend/src/components/ProductList/index.js
+++ frontend/src/components/ProductList/index.js
@@ -26,50 +26,6 @@ function ProductList() {
                         <p className="product_text">incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
                     </div>
                 </div>
-<<<<<<< HEAD
-                <div className="product_section_2 layout_padding">
-                    <div className="row">
-                        <div className="col-lg-3 col-sm-6">
-                           <Product name={"Product 1"} desc={"Day la Nhi"} price={30} />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            <Product />
-                        </div>
-=======
                 <div class="product_section_2 layout_padding">
                     <div class="row">
                         {products.map(product => (
@@ -77,7 +33,6 @@ function ProductList() {
                             <Product product={product} />
                             </div>
                         ))}
->>>>>>> daea3fb1f0303dcbcc8095230af56fb5e8eccb67
                     </div>
                     <div className="seemore_bt"><a href="#">See More</a></div>
                 </div>
