a5b99cd7c02020de5b97446c4f340026b7956b6f
Minhbinh0206 <haminh4235@gmail.com>
2024-04-06 12:16:43 +0700

    mmm

--- frontend/src/components/Contact/index.js
+++ frontend/src/components/Contact/index.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 
 function Contact() {
+   const [configs, setConfigs] = useState([]);
+
+   useEffect(() => {
+      fetch("http://localhost:3300/configs")
+         .then(res => res.json())
+         .then(data => {
+            setConfigs(data);
+         })
+         .catch(err => {
+            setConfigs([]);
+         });
+   }, []);
+
+   console.log(configs);
+
     return (
         <div className="contact_section layout_padding">
         <div className="container">
            <div className="row">
               <div className="col-md-6">
                  <h1 className="contact_taital">Get In Touch</h1>
-                 <p className="contact_text">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation  eu </p>
+                 <p className="contact_text">{configs[1] && configs[1].value}</p>
               </div>
               <div className="col-md-6">
                  <div className="contact_main">
